b843eedad4b7629b63640e3cb734aaa18cce2804
Infinatum <sean.emmett4@gmail.com>
2016-07-21 20:49:39 +0100

    Fixed Violet Bug for coloured glass

--- mod_files/coloured_glass.lua
+++ mod_files/coloured_glass.lua
@@ -7,7 +7,7 @@ minetest.register_node("medieval:glass_square", {
 		sunlight_propagates = true,
 		sounds = default.node_sound_glass_defaults(),
 		groups = {cracky=3,oddly_breakable_by_hand=3},
-	}) 
+	})
 
 minetest.register_node("medieval:glass_round", {
 		description = "Round Glass",
@@ -18,12 +18,12 @@ minetest.register_node("medieval:glass_round", {
 		sunlight_propagates = true,
 		sounds = default.node_sound_glass_defaults(),
 		groups = {cracky=3,oddly_breakable_by_hand=3},
-	}) 
+	})
 
 
 
 local function register_square(name, description, colorCode, dye)
-	
+
 	minetest.register_node("medieval:glass_square_"..name, {
 		description = "Square ".. description .." Glass",
 		drawtype = "glasslike",
@@ -33,27 +33,27 @@ local function register_square(name, description, colorCode, dye)
 		sunlight_propagates = true,
 		sounds = default.node_sound_glass_defaults(),
 		groups = {cracky=3,oddly_breakable_by_hand=3},
-	}) 
+	})
 	-- todo register craft reziep for round <name> glass here
-	
+
 	minetest.register_craft({
 		type = "shapeless",
 		output = "medieval:glass_square_"..name ,
 		recipe =  {"medieval:glass_square","dye:"..dye,},
 	})
-	
+
 	minetest.register_craft({
 		type = "cooking",
 		output = "medieval:glass_square",
 		recipe = "medieval:glass_square_"..name,
 		cooktime = 10,
 	})
-	
+
 
 end
 
 local function register_round(name, description, colorCode, dye)
-	
+
 	minetest.register_node("medieval:glass_round_"..name, {
 		description = "Round ".. description .." Glass",
 		drawtype = "glasslike",
@@ -63,28 +63,28 @@ local function register_round(name, description, colorCode, dye)
 		sunlight_propagates = true,
 		sounds = default.node_sound_glass_defaults(),
 		groups = {cracky=3,oddly_breakable_by_hand=3},
-	}) 
+	})
 	-- todo register craft reziep for round <name> glass here
-	
+
 	minetest.register_craft({
 		type = "shapeless",
 		output = "medieval:glass_round_"..name ,
 		recipe = {"medieval:glass_round","dye:"..dye,},
 	})
-	
-		
+
+
 	minetest.register_craft({
 		type = "cooking",
 		output = "medieval:glass_round",
 		recipe = "medieval:glass_round_"..name,
 		cooktime = 10,
 	})
-	
-	
+
+
 end
 
 local function register_dimond(name, description, colorCode, dye)
-	
+
 	minetest.register_node("medieval:glass_dimond_"..name, {
 		description = "Dimond "..description.." Glass",
 		drawtype = "glasslike",
@@ -94,40 +94,40 @@ local function register_dimond(name, description, colorCode, dye)
 		sunlight_propagates = true,
 		sounds = default.node_sound_glass_defaults(),
 		groups = {cracky=3,oddly_breakable_by_hand=3},
-	}) 
-	-- todo register craft reziep for dimond <name> glass here 
-	
+	})
+	-- todo register craft reziep for dimond <name> glass here
+
 	minetest.register_craft({
-		type = "shapeless",	
+		type = "shapeless",
 		output = "medieval:glass_dimond_"..name ,
 		recipe = {"darkage:glass","dye:"..dye,},
-		
+
 	})
-	
+
 		minetest.register_craft({
 		type = "cooking",
 		output = "darkage:glass",
 		recipe = "medieval:glass_dimond_"..name,
 		cooktime = 10,
 	})
-	
+
 	minetest.register_alias("medieval_craft:medieval_glass_"..name , "medieval:glass_dimond_"..name)
-	
+
 end
 
 colours = {
-	-- RGB Prime Colours -- 
-	{name="red", code="FF000099", description="Red", dye="red"}, 
+	-- RGB Prime Colours --
+	{name="red", code="FF000099", description="Red", dye="red"},
 	{name="green", code="00FF0099", description="Green", dye="green"},
 	{name="blue", code="0000FF99", description="Blue", dye="blue"},
-	-- RGB Secondary Colour -- 
+	-- RGB Secondary Colour --
 	{name="yellow", code="FFFF0099", description="Yellow", dye="yellow"},
 	{name="magenta", code="FF00FF99", description="Magenta", dye="magenta"},
 	{name="cyan", code="00FFFF99", description="Cyan", dye="cyan"},
 	-- RGB Other Colours --
 	{name="orange", code="E5940099", description="Orange", dye="orange"},
 	{name="darkgreen", code="004C0099", description="Dark Green", dye="dark_green"},
-	{name="purple", code="80008099", description="Purple", dye="purple"},
+	{name="purple", code="80008099", description="Purple", dye="violet"},
 	{name="pink", code="FFC0CB99", description="Pink", dye="pink"},
 	{name="brown", code="732c0b99", description="Brown", dye="brown"},
 	-- MonoChrome --
@@ -140,5 +140,5 @@ for i,colour in ipairs(colours) do
 	register_round(colour.name, colour.description, colour.code, colour.dye)
 	register_square(colour.name, colour.description, colour.code, colour.dye)
 	register_dimond(colour.name, colour.description, colour.code, colour.dye)
-	-- register whatever here 
+	-- register whatever here
 end
